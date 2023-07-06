72b61456fd1c991afbe4e777e29e64ee80438015
Ilgner <ilgner.nascimento@plurya.com>
2023-07-06 17:59:31 -0300

    Crinando banco

--- src/Helper/EntityManagerFactory.php
+++ src/Helper/EntityManagerFactory.php
@@ -1,29 +1,33 @@
 <?php
 
 namespace App\Helper;
-
+// bootstrap.php
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMSetup;
-use Doctrine\ORM\Tools\Setup;
 
-class EntityManagerFactory {
-    /**
-     * @return EntityManagerInterface
-     * @throws \Doctrine\ORM\EntityNotFoundException
-     */
-    public function getEntityManager(): EntityManagerInterface {
-        $rootDir = __DIR__. '/../..';
-        $dbParams = [
+require_once dirname(dirname(__DIR__)) . "/vendor/autoload.php";
+
+
+class EntityManagerFactory
+{
+    // Create a simple "default" Doctrine ORM configuration for Attributes
+    public static function getEntityManager()
+    {
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            paths: array(dirname(__DIR__)."/Entity"),
+            isDevMode: true,
+        );
+
+
+        // configuring the database connection
+        $connection = DriverManager::getConnection([
             'driver' => 'pdo_sqlite',
-            'path' => $rootDir . '/var/data/banco.sqlite',
-        ];
-        $config = ORMSetup::createAttributeMetadataConfiguration([$rootDir . '/src'], true);
-        $connection = DriverManager::getConnection($dbParams, $config);
+            'path' => __DIR__ . '/var/data/db.sqlite',
+        ], $config);
+
+        // obtaining the entity manager
         $entityManager = new EntityManager($connection, $config);
         return $entityManager;
-        //return $rootDir;
-        //$config = Setup::createAnnotationMetadataConfiguration([$rootDir . '/src'], true);
     }
 }
--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Helper\EntityManagerFactory;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$enityManagerFactory = new EntityManagerFactory();
-$enityManager = $enityManagerFactory->getEntityManager();
-
-var_dump($enityManager);
